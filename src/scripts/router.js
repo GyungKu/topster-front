cf78fe6f2c510d707b0a05e781c18a006e8e755a
gk <iba3374@naver.com>
2024-01-19 21:18:29 +0900

    refactor: 로그인 기능 수정

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -9,6 +9,10 @@ import TopsterRegister from "@/pages/TopsterRegister.vue";
 import Board from "@/pages/Board.vue";
 import MyTopster from "@/pages/MyTopster.vue";
 import PostEdit from "@/pages/PostEdit.vue";
+import MyProfile from "@/pages/MyProfile.vue";
+import ProfileEdit from "@/pages/ProfileEdit.vue";
+import MyPosts from "@/pages/MyPosts.vue";
+import TopsterList from "@/pages/TopsterList.vue";
 
 const routes = [
   {name: 'home', path: '/', component: Home},
@@ -22,6 +26,10 @@ const routes = [
   {name: 'board', path: '/posts', component: Board},
   {name: 'myTopster', path: '/myTopster', component: MyTopster},
   {name: 'postEdit', path: '/posts/:postId/edit', component: PostEdit},
+  {name: 'myProfile', path: '/myprofile', component: MyProfile},
+  {name: 'profileEdit', path: '/myprofile/edit', component: ProfileEdit},
+  {name: 'myPosts', path: '/posts/my', component: MyPosts},
+  {name: 'topsters', path: '/topsters', component: TopsterList},
 ]
 
 const router = createRouter({
