badcdfc9f6279307a5d2ce048d3992745051ee65
gk <iba3374@naver.com>
2024-01-20 20:14:44 +0900

    feat: 이메일 인증 타이머 추가

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,40 +1,49 @@
 <template>
   <div class="form-signup w-100 m-auto">
-      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-      <div class="form-floating">
-        <input type="username" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="username">
-        <label for="floatingInput">아이디를 입력해 주세요</label>
-      </div>
-      <div class="form-floating">
-        <input type="nickname" class="form-control" id="floatingInput" placeholder="nickname" v-model="nickname">
-        <label for="floatingInput">닉네임을 입력해 주세요</label>
-      </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="password">
+      <input type="username" class="form-control" id="floatingInput" placeholder="name@example.com"
+             v-model="username">
+      <label for="floatingInput">아이디를 입력해 주세요</label>
+    </div>
+    <div class="form-floating">
+      <input type="nickname" class="form-control" id="floatingInput" placeholder="nickname"
+             v-model="nickname">
+      <label for="floatingInput">닉네임을 입력해 주세요</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+             v-model="password">
       <label for="floatingPassword">비밀번호를 입력해 주세요</label>
     </div>
     <div class="d-flex align-items-center">
       <div class="form-floating flex-grow-1">
-        <input type="email" class="form-control" id="floatingInput" placeholder="email" v-model="email">
+        <input type="email" class="form-control" id="floatingInput" placeholder="email"
+               v-model="email">
         <label for="floatingInput">이메일을 입력해 주세요</label>
       </div>
       <button class="btn btn-outline-secondary ms-2" @click="checkEmail">이메일 인증</button>
     </div>
+    <div>
+      <p class="timer" v-if="timer">{{ minutes }}:{{ seconds }}</p>
+      <p class="timer" v-if="timeOff">시간이 초과 되었습니다.</p>
+    </div>
     <div class="d-flex align-items-center mt-2">
       <div class="form-floating flex-grow-1">
-        <input type="text" class="form-control" id="floatingVerificationCode" placeholder="Verification Code" v-model="certification">
+        <input type="text" class="form-control" id="floatingVerificationCode"
+               placeholder="Verification Code" v-model="certification">
         <label for="floatingVerificationCode">인증번호를 입력해 주세요</label>
       </div>
-<!--      <button class="btn btn-outline-secondary ms-2" @click="verifyCode">인증번호 확인</button>-->
+      <!--      <button class="btn btn-outline-secondary ms-2" @click="verifyCode">인증번호 확인</button>-->
     </div>
     <div class="form-floating">
-      <input type="intro" class="form-control" id="floatingInput" placeholder="intro" v-model="intro">
+      <input type="intro" class="form-control" id="floatingInput" placeholder="intro"
+             v-model="intro">
       <label for="floatingInput">자기소개를 입력해 주세요</label>
     </div>
 
-      <button class="btn btn-primary w-100 py-2" @click="submit()">회원 가입</button>
-      <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+    <button class="btn btn-primary w-100 py-2" @click="submit()">회원 가입</button>
   </div>
 </template>
 
@@ -51,14 +60,18 @@ export default {
     }
   },
 
-  data()  {
-    return{
-      username:"",
-          nickname:"",
-          password:"",
-          email:"",
-          certification:"",
-          intro:""
+  data() {
+    return {
+      username: "",
+      nickname: "",
+      password: "",
+      email: "",
+      certification: "",
+      intro: "",
+      timer: null,
+      minutes: 4,
+      seconds: 59,
+      timeOff: false,
     }
   },
   methods: {
@@ -66,6 +79,7 @@ export default {
       axios.post("/users/mail?email=" + encodeURIComponent(this.email))
       .then(() => {
         window.alert("인증번호가 발송되었습니다.");
+        this.startTimer();
       })
       .catch((err) => {
         console.error(err);
@@ -75,12 +89,12 @@ export default {
 
     submit() {
       const form = {
-        username:this.username,
-        nickname:this.nickname,
-        password:this.password,
-        email:this.email,
-        certification:this.certification,
-        intro:this.intro,
+        username: this.username,
+        nickname: this.nickname,
+        password: this.password,
+        email: this.email,
+        certification: this.certification,
+        intro: this.intro,
       }
       axios.post("/users/signup", form)
       .then((res) => {
@@ -99,8 +113,8 @@ export default {
 
     verifyCode() {
       const form = {
-        email:this.email,
-        certification:this.certification,
+        email: this.email,
+        certification: this.certification,
       }
       axios.post("/users/mail/certification", form)
       .then(() => {
@@ -110,7 +124,41 @@ export default {
         alert(err.response.data.message);
       });
     },
-  }
+
+    startTimer() {
+      // 타이머가 이미 실행 중이라면 초기화
+      if (this.timer) {
+        clearInterval(this.timer);
+        this.minutes = 4;
+        this.seconds = 59;
+      }
+      // 1초마다 카운트다운
+      this.timer = setInterval(() => {
+        if (this.seconds === 0) {
+          if (this.minutes === 0) {
+            // 5분이 모두 지났다면 타이머를 멈춤
+            clearInterval(this.timer);
+            this.timer = null;
+            this.timeOff = true;
+          } else {
+            this.minutes--;
+            this.seconds = 59;
+          }
+        } else {
+          this.seconds--;
+        }
+      }, 1000);
+    },
+
+  },
+
+  beforeUnmount() {
+    // 컴포넌트가 사라지기 전에 타이머를 꼭 정리해줍니다.
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  },
+
 }
 
 
@@ -135,6 +183,7 @@ export default {
 .form-control {
   padding-right: 10px; /* 예시: 작은 값으로 조정 */
 }
+
 .form-floating {
   width: 100%;
 }
@@ -156,5 +205,9 @@ export default {
 
 .btn-outline-secondary {
   width: 100px;
+}
+
+.timer {
+  color: red;
 }
 </style>
